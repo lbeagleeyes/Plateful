6275ee35bcd4daa945962651282adf2101dc7ef3
Laura B <laurabermudezg@gmail.com>
2019-06-08 23:51:23 -0700

    Added association between calendarRecipe and User

--- models/calendarRecipe.js
+++ models/calendarRecipe.js
@@ -1,10 +1,27 @@
 module.exports = function(sequelize, DataTypes) {
   var CalendarRecipe = sequelize.define("CalendarRecipe", {
-    recipeName: DataTypes.STRING,
-    recipeId: DataTypes.INTEGER,
+    recipeName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    recipeId: { 
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }, 
     date: DataTypes.DATE,
     mealtime: DataTypes.STRING,
-    UserId:DataTypes.INTEGER
+    UserId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   });
+
+  CalendarRecipe.associate = function(models) {
+    CalendarRecipe.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
   return CalendarRecipe;
 };
--- models/user.js
+++ models/user.js
@@ -1,6 +1,42 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
-    name: DataTypes.STRING
+    firstname: {
+      type: DataTypes.STRING,
+      notEmpty: true
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      notEmpty: true
+    },
+    username: {
+      type: DataTypes.TEXT
+    },
+    about: {
+      type: DataTypes.TEXT
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    last_login: {
+      type: DataTypes.DATE
+    },
+    status: {
+      type: DataTypes.ENUM('active', 'inactive'),
+      defaultValue: 'active'
+    }
   });
+
+  User.associate = function(models) {
+    User.hasMany(models.CalendarRecipe, {
+      onDelete: "cascade"
+    });
+  };
   return User;
 };
