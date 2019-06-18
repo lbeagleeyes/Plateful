4963342b2cf2a463c94a853f7121d89032a4d3cf
Laura B <laurabermudezg@gmail.com>
2019-06-17 17:25:45 -0700

    merging get current user id

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,150 +1,117 @@
 var NUM_DAYS = 3;
 var currentUserId = 1;
+
 $(document).ready(function () {
-  var datesToDisplay = getDatesToDisplay();
-  datesToDisplay.forEach(day => {
-    displayDay(day);
-    displayMealtime("Breakfast", day);
-    displayMealtime("Lunch", day);
-    displayMealtime("Dinner", day);
+  $.ajax({
+    type: "GET",
+    url: '/api/uid',
+    success: function (response) {
+      currentUserId = response;
+      console.log("calendar.js stored userID in variable userID = " + response);
+      var datesToDisplay = getDatesToDisplay();
+      datesToDisplay.forEach(day => {
+        displayDay(day);
+        displayMealtime("Breakfast", day);
+        displayMealtime("Lunch", day);
+        displayMealtime("Dinner", day);
+      });
+    }
   });
-
-  // console.log("=============================================\n=============================================");
-  // console.log("=============================================\n=============================================");
-  // console.log("=============================================\n=============================================");
-  // console.log("Index JSON reads: ");
-  // console.log(res);
-  // console.log("=============================================\n=============================================");
-  // console.log("=============================================\n=============================================");
-  // console.log("=============================================\n=============================================");
-
-  //get today's date, save today, tomorrow and the day after tomorrow
-  //get recipes for the current user for the three days
-  //display the dates
-  //display recipes in calendar according to dates and mealtimes
-  var userID;
-
-  getUID().then(function(resp) {
-    userID = resp;
-
-    console.log("calendar.js stored userID in variable userID = " + userID);
-  });
-
-  // Due to asynchronicity, the below will produce 'UNDEFINED'
 });
 
-function getUID() {
-  $.ajax({
-    type: "GET",
-    url: `/api/uid`,
-    success: function (response) {
-      console.log("=============================================\n=============================================");
-      console.log("=============================================\n=============================================");
-      console.log("=============================================\n=============================================");
-      // console.log("Frontend reads user ID as: ");
-      // console.log(response)
-      console.log("=============================================\n=============================================");
-      console.log("=============================================\n=============================================");
-      console.log("=============================================\n=============================================");
-
-      return response;
-    }
-  })
-};
-
-function displayDay(day) {
-  var dayCol = new $('<div>', {
-    class: "col s4 day",
-    text: day.format("MMMM Do YYYY")
-  });
-  $('#days').append(dayCol);
-}
-
-
-function displayMealtime(mealtime, day) {
-  var date = day.format("YYYY-MM-DD");
-  console.log("Date being queried: " + date);
-  $.ajax({
-    type: "GET",
-    url: `/calendarRecipesInDayForMealtime/${currentUserId}/${date}/${mealtime}`,
-    success: function (response) {
-      console.log(date + " - " + mealtime + " : " + JSON.stringify(response));
-      var row = "#" + mealtime.toLowerCase();
-      if (response.length === 0) {
-        $(row).append($('<div>', {
-          class: "col s4 calendarCel", 
-          text: ""
-        }).on("drop", function (ev) {
-          ev.preventDefault();
-          var recipe = ev.originalEvent.dataTransfer.getData("recipe");
-          var elementId = ev.originalEvent.dataTransfer.getData("elementId");
-          ev.target.appendChild(document.getElementById(elementId));
-          saveRecipe(recipe, mealtime, date);
-        }).on("dragover", function(ev){
-          ev.preventDefault();
-        }));
-      } else {
-        console.log(response[0]);
-        var recipeCell = new $('<div>', {
-          class: "col s4"
-        });
-        var card = createCard(response[0]);
-        recipeCell.append(card);
-
-        $(row).append(recipeCell);
-      }
-    }
-  });
-}
-
-function saveRecipe(recipe, mealtime, date){
-  //ajax post to create recipe
-  $.ajax({
-    type: "POST",
-    url: "/api/newCalendarRecipe",
-    data: {userId: currentUserId, recipe: recipe, mealtime: mealtime, date:date},
-    success: function(){
-      console.log("recipe saved to calendar");
-    }
-  });
-}
-
-function getUserRecipes(day) {
-  $.ajax({
-    type: "GET",
-    url: `/calendarRecipesInDay/${currentUserId}/${day.format("YYYY-MM-DD")}`,
-    success: function (response) {
-      displayRecipes(day, response);
-      return response;
-    }
-  });
-}
-
-
-// function getUserRecipesInDateRange(datesToDisplay) {
-//   $.ajax({
-//     type: "GET",
-//     url: `/calendarRecipes/${currentUserId}/${datesToDisplay[0].format("YYYY-MM-DD")}/${datesToDisplay[datesToDisplay.length - 1].format("YYYY-MM-DD")}`,
-//     success: function (response) {
-//       displayRecipes(response);
-//       return response;
-//     }
-//   });
-// }
-
-// function displayRecipes(day, userRecipes) {
-//   //display date in calendar
-//   for (var i = 0; i < userRecipes.length; i++) {
-//     //create card for each recipe and display in day column
-
-//   }
-
-// }
-
-function getDatesToDisplay() {
-  var dates = [];
-  for (var i = 0; i < NUM_DAYS; i++) {
-    dates.push(moment().add(i, "days"));
+  function displayDay(day) {
+    var dayCol = new $('<div>', {
+      class: "col s4 day",
+      text: day.format("MMMM Do YYYY")
+    });
+    $('#days').append(dayCol);
   }
-  return dates;
-}
+
+
+  function displayMealtime(mealtime, day) {
+    var date = day.format("YYYY-MM-DD");
+    console.log("Date being queried: " + date);
+    $.ajax({
+      type: "GET",
+      url: `/calendarRecipesInDayForMealtime/${currentUserId}/${date}/${mealtime}`,
+      success: function (response) {
+        console.log(date + " - " + mealtime + " : " + JSON.stringify(response));
+        var row = "#" + mealtime.toLowerCase();
+        if (response.length === 0) {
+          $(row).append($('<div>', {
+            class: "col s4 calendarCel",
+            text: ""
+          }).on("drop", function (ev) {
+            ev.preventDefault();
+            var recipe = ev.originalEvent.dataTransfer.getData("recipe");
+            var elementId = ev.originalEvent.dataTransfer.getData("elementId");
+            ev.target.appendChild(document.getElementById(elementId));
+            saveRecipe(recipe, mealtime, date);
+          }).on("dragover", function (ev) {
+            ev.preventDefault();
+          }));
+        } else {
+          console.log(response[0]);
+          var recipeCell = new $('<div>', {
+            class: "col s4"
+          });
+          var card = createCard(response[0]);
+          recipeCell.append(card);
+
+          $(row).append(recipeCell);
+        }
+      }
+    });
+  }
+
+  function saveRecipe(recipe, mealtime, date) {
+    //ajax post to create recipe
+    $.ajax({
+      type: "POST",
+      url: "/api/newCalendarRecipe",
+      data: { userId: currentUserId, recipe: recipe, mealtime: mealtime, date: date },
+      success: function () {
+        console.log("recipe saved to calendar");
+      }
+    });
+  }
+
+  function getUserRecipes(day) {
+    $.ajax({
+      type: "GET",
+      url: `/calendarRecipesInDay/${currentUserId}/${day.format("YYYY-MM-DD")}`,
+      success: function (response) {
+        displayRecipes(day, response);
+        return response;
+      }
+    });
+  }
+
+
+  // function getUserRecipesInDateRange(datesToDisplay) {
+  //   $.ajax({
+  //     type: "GET",
+  //     url: `/calendarRecipes/${currentUserId}/${datesToDisplay[0].format("YYYY-MM-DD")}/${datesToDisplay[datesToDisplay.length - 1].format("YYYY-MM-DD")}`,
+  //     success: function (response) {
+  //       displayRecipes(response);
+  //       return response;
+  //     }
+  //   });
+  // }
+
+  // function displayRecipes(day, userRecipes) {
+  //   //display date in calendar
+  //   for (var i = 0; i < userRecipes.length; i++) {
+  //     //create card for each recipe and display in day column
+
+  //   }
+
+  // }
+
+  function getDatesToDisplay() {
+    var dates = [];
+    for (var i = 0; i < NUM_DAYS; i++) {
+      dates.push(moment().add(i, "days"));
+    }
+    return dates;
+  }
